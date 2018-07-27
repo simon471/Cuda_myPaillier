b47ca47755d64956eeec87c7503ddc5a82c3028f
simon471 <simonchiu1106@gmail.com>
2018-07-26 21:54:14 -0400

    modified source.cpp

--- myPaillier/Source.cpp
+++ myPaillier/Source.cpp
@@ -7,12 +7,18 @@ int main() {
 	
 	cout << "program starting..." << endl;
 
+	cout << "Declaring variables..." << endl;
 	pubkey publickey;
 	prvkey privatekey;
 	cout << "Variables declared..." << endl;
 
-	setup(publickey, privatekey);
+	cout << "Initializing keys..." << endl;
+	initial(&publickey, &privatekey);
+	cout << "Keys initialized..." << endl;
 
+	cout << "Setting up key values..." << endl;
+	setup(&publickey, &privatekey);
+	cout << "Key generation done..." << endl;
 
 	cout << "program ending..." << endl;
 
